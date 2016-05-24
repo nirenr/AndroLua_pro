1009c3689fc14a3b4b334f86c39df7d4dd3a41c9
nirenr <nirenr@sina.com>
2016-05-24 12:00:09 +0800

    改进Java方法浏览器支持查看事件，gettwr与setter

--- assets/java.lua
+++ assets/java.lua
@@ -20,9 +20,9 @@ import "android.content.*"
 cm=activity.getSystemService(activity.CLIPBOARD_SERVICE)
 
 function copy(str)
-    local cd = ClipData.newPlainText("label",str)
-    cm.setPrimaryClip(cd)
-    Toast.makeText(activity,"已复制的剪切板",1000).show()
+  local cd = ClipData.newPlainText("label",str)
+  cm.setPrimaryClip(cd)
+  Toast.makeText(activity,"已复制的剪切板",1000).show()
 end
 
 
@@ -38,6 +38,11 @@ clist.onItemClick=function(l,v)
   local class=luajava.bindClass(s)
   curr_class=class
   local t={}
+  local fs={}
+  local ms={}
+  local es={}
+  local ss={}
+  local gs={}
   local super=class.getSuperclass()
   super=super and " extends "..tostring(super.getName()) or ""
   table.insert(t,tostring(class)..super)
@@ -47,20 +52,49 @@ clist.onItemClick=function(l,v)
   for n=0,#cs-1 do
     table.insert(t,tostring(cs[n]))
   end
-  
-  local fs=class.getFields()
-  curr_fs=fs
-  table.insert(t,"公有字段")
-  for n=0,#fs-1 do
-    table.insert(t,tostring(fs[n]))
+
+
+
+  curr_ms=class.getMethods()
+  for n=0,#curr_ms-1 do
+    local str=tostring(curr_ms[n])
+    table.insert(ms,str)
+    local e1=str:match("%.setOn(%a+)Listener")
+    local s1,s2=str:match("%.set(%a+)(%([%a$%.]+%))")
+    local g1,g2=str:match("([%a$%.]+) [%a$%.]+%.get(%a+)%(%)")
+    if e1 then
+      table.insert(es,"on"..e1)
+    elseif s1 then
+      table.insert(ss,s1..s2)
+    end
+    if g1 then
+      table.insert(gs,string.format("(%s)%s",g1,g2))
+    end
+  end
+  table.insert(t,"公有事件")
+  for k,v in ipairs(es) do
+    table.insert(t,v)
+  end
+  table.insert(t,"公有getter")
+  for k,v in ipairs(gs) do
+    table.insert(t,v)
+  end
+  table.insert(t,"公有setter")
+  for k,v in ipairs(ss) do
+    table.insert(t,v)
   end
 
-  local ms=class.getMethods()
-  curr_ms=ms
-  table.insert(t,"公有方法")
-  for n=0,#ms-1 do
-    table.insert(t,tostring(ms[n]))
+  curr_fs=class.getFields()
+  table.insert(t,"公有字段")
+  for n=0,#curr_fs-1 do
+    table.insert(t,tostring(curr_fs[n]))
   end
+
+  table.insert(t,"公有方法")
+  for k,v in ipairs(ms) do
+    table.insert(t,v)
+  end
+
   dlg.Title=tostring(s)
   curt_adapter=adapter(t)
   mlist.setAdapter(curt_adapter)
@@ -77,9 +111,9 @@ mlist.onItemLongClick=function(l,v)
   local s=tostring(v.Text)
   if s:find("%(") then
     s=s:match("(%w+)%(")
-    else
+  else
     s=s:match("(%w+)$")
-    end
+  end
   copy(s)
   return true
 end
